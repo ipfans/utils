836cded3e0d28b0579aec11b05154c92344f4b4c
ipfans <ipfanscn@gmail.com>
2012-07-21 10:33:55 +0800

    Reorganize directory
    Reorganize directory, and add IDAPython Script to utils

---
+++ IDA/dejunk.py
@@ -0,0 +1,136 @@
+# dejunk.py
+#
+# ipfans modify from Gregory Newman
+
+from idaapi import *
+
+junk_patterns = [
+        {
+            'pattern':[0xF2,0xEB],
+            'fillwith':[0x90,0xEB],
+        },
+        {
+            'pattern':[0xF3, 0xEB],
+            'fillwith':[0x90, 0xEB],
+        },
+        {
+            'pattern':[0x65,0xEB],
+            'fillwith':[0x90,0xEB],
+        },
+        {
+            'pattern':[0x64, 0xEB],
+            'fillwith':[0x90, 0xEB],
+        },
+        {
+            'pattern':[0x36,0xEB],
+            'fillwith':[0x90,0xEB],
+        },
+        {
+            'pattern':[0x3E, 0xEB],
+            'fillwith':[0x90, 0xEB],
+        },
+        {
+            'pattern':[0x26,0xEB],
+            'fillwith':[0x90,0xEB],
+        },
+        {
+            'pattern':[0x2E, 0xEB],
+            'fillwith':[0x90, 0xEB],
+        }, 
+        {
+            'pattern':[0x74, 0x04, 0x75, 0x02, '??', '??'],
+            'fillwith':[0x90, 0x90, 0x90, 0x90, 0x90, 0x90],
+        },
+        {
+            'pattern':[0x75, 0x04, 0x74, 0x02, '??', '??'],
+            'fillwith':[0x90, 0x90, 0x90, 0x90, 0x90, 0x90],
+        },
+    ]
+
+end_patterns = [
+        [0xC3],
+        [0xC2,'??','??'],
+        [0xFF, 0x64, 0x24, 0xFC],
+    ]
+
+
+def match_pattern(ea):
+
+    for pattern in junk_patterns:
+        # create blank opcode list for comparison
+        opcode_bytes = []
+        opcode_iterator = ea
+        for byte in pattern['pattern']:
+            if byte == '??':
+                opcode_bytes.append(byte) # wildcard byte doesn't mater so append wildcard for matching
+                continue
+            opcode_bytes.append(get_byte(opcode_iterator))
+            opcode_iterator += 1
+        if opcode_bytes == pattern['pattern']:
+            return pattern
+
+    return None
+
+def match_end(ea):
+    for pattern in end_patterns:
+        opcode_bytes = []
+        opcode_iterator = ea
+        for byte in pattern:
+            if byte == '??':
+                opcode_bytes.append(byte) # wildcard byte doesn't matter so append wildcard for matching
+                continue
+            opcode_bytes.append(get_byte(opcode_iterator))
+            opcode_iterator += 1
+        if opcode_bytes == pattern:
+            return True
+
+    return False
+
+def patch_db(ea, pattern):
+    opcode_iterator = ea
+    for byte in pattern['fillwith']:
+        put_byte(opcode_iterator, byte)
+        opcode_iterator += 1
+
+# max_size is set to 1024 to avoid excessive dejunking or not finding any end
+def find_end(ea, max_size=0x400):
+    iterator = ea
+    ea_end = ea+max_size
+    while iterator != 0xFFFFFFFF:
+        if match_end(iterator) == True:
+            return iterator
+        iterator = next_head(iterator, ea_end)
+
+    return False
+
+
+def dejunk_selection():
+    selection_start = SelStart()
+    selection_end = SelEnd()
+    selection_size = selection_end - selection_start
+    print "Dejunking %X - %X" % (selection_start, selection_end)
+    dejunk(selection_start, selection_end)
+    do_unknown_range(selection_start, selection_size, DOUNK_SIMPLE) # un-analyze code
+    auto_make_code(selection_start) # re-analyze to reflect patches
+
+def dejunk_until_end():
+    ea_start = get_screen_ea()
+    ea_end = find_end(ea_start)
+    print "Dejunking %X - %X" % (ea_start, ea_end)
+    dejunk(ea_start, ea_end)
+    do_unknown_range(ea_start, ea_end, DOUNK_SIMPLE) # un-analyze code
+    auto_make_code(ea_start) # re-analyze to reflect patches
+    
+
+def dejunk(ea_start, ea_end):
+    junk_iterator = ea_start
+
+    while junk_iterator != 0xFFFFFFFF:
+        pattern = match_pattern(junk_iterator)
+        if pattern != None:
+            patch_db(junk_iterator, pattern)
+        junk_iterator = next_head(junk_iterator, ea_end)
+
+    return
+
+dejunk_selection()
---
+++ Others/calc_api_name_hash.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+calc_api_name_hash.py
+
+TODO
+"""
+__author__      = 'Binjo'
+__version__     = '0.3'
+__date__        = '2008-10-28 15:25:46'
+__description__ = "Calculate api names' hash or Search name via hash."
+
+import os, sys
+import pefile
+import optparse
+import sqlite3
+
+FILEZ       = {
+    'kernel32'  :   'c:/windows/system32/kernel32.dll',
+    'ntdll'     :   'c:/windows/system32/ntdll.dll',
+    'shell32'   :   'c:/windows/system32/shell32.dll',
+    'user32'    :   'c:/windows/system32/user32.dll',
+    'urlmon'    :   'c:/windows/system32/urlmon.dll',
+    'ws2_32'    :   'c:/windows/system32/ws2_32.dll',
+    }
+
+db = sqlite3.connect( 'api_name_hash.db' )
+db.isolation_level = None       # auto commit
+cr = db.cursor()
+if ( (not os.path.isfile('api_name_hash.db')) or
+     os.path.getsize('api_name_hash.db') == 0 ):
+    # api_hash : XXXXXXXX
+    # api_name : name-file-offset
+    cr.execute("""
+        create table hashes (api_hash text, api_name text)""")
+
+def calc_hash(src, off):
+    """
+                push    esi
+                xor     esi, esi
+loc_108A:
+                movsx   edx, byte ptr [eax]
+                cmp     dh, dl
+                jz      short loc_1099
+                ror     esi, 0Dh
+                add     esi, edx
+                inc     eax
+                jmp     short loc_108A
+
+    """
+    if src is None: return( -1, src)
+
+    h = 0
+    for x in src.rstrip('\n'):
+        h  = (h >> int(off)) ^ (0xFFFFFFFF & (h << (32 - int(off))))
+        h += ord(x)
+    return (h, src)
+
+def main():
+    """TODO
+    """
+    opt = optparse.OptionParser( usage='usage: %prog [options]\n' + __description__, version='%prog ' + __version__)
+    opt.add_option( '-s', '--search', help='hash string to search' )
+    opt.add_option( '-o', '--offset', help='offset for the calculation' )
+    opt.add_option( '-f', '--file',   help='file name' )
+
+    (opts, args) = opt.parse_args()
+
+    if len(args) != 0:
+        opt.print_help()
+        print '  -f is as follows:'
+        for k, v in FILEZ.iteritems():
+            print '    %s -> %s' % ( k, v )
+        exit(-1)
+
+    keyname  = opts.file
+    searchee = opts.search
+    offset   = opts.offset
+
+    # for search
+    if searchee is not None:
+        cr.execute("select api_name from hashes where api_hash=?",
+                   ("%08X" % int( searchee, 16 ), ))
+        print "%s, %08X" % (cr.fetchone()[0], int( searchee, 16 ))
+        return
+
+    # insert table
+    print 'offset %s, %s' % ( offset, keyname )
+
+    def filter_rule(x):
+        """
+
+        Arguments:
+        - `x`:
+        """
+        if keyname is not None:
+            return x == keyname
+        else:
+            return True
+
+    files = filter( filter_rule, FILEZ.keys() )
+
+    for f in files:
+        dll = FILEZ.get( f, '' )
+        if dll != '':
+            pe = pefile.PE(dll)
+
+        for s in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+            v, n = calc_hash( s.name, offset )
+            print '%08X, %s' % ( v, n )
+            if v == -1: continue
+            cr.execute("""
+                insert into hashes
+                values ( ?, ? )""", ("%08X" % v, "%s-%s-%s" % (n, keyname, offset)) )
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
+#-------------------------------------------------------------------------------
+# EOF
